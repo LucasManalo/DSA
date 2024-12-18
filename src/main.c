fa22f612a1445297e9b7aaf41fcab52fd1509c19
Lucas Manalo <lucasmanalo@gmail.com>
2024-12-18 02:15:51 -0500

    first commit

--- src/main.c
+++ src/main.c
@@ -9,28 +9,5 @@ void print_int(void* data) {
 }
 
 int main() {
-    LinkedList* list = new_linked_list();
-    int arr[5] = {1, 2, 3, 4, 5};
-
-    push_head(list, &arr[2]);
-    push_head(list, &arr[1]);
-    push_head(list, &arr[0]);
-    push_tail(list, &arr[3]);
-    push_tail(list, &arr[4]);
-
-    reverse_list(list);
-    print_list(list, print_int);
-
-    remove_node(list, 4);
-    remove_node(list, 2);
-    remove_node(list, 0);
-
-
-    // printf("%d\n", *(int*)get_node(list, 2));
-
-    print_list(list, print_int);
-
-    free_list(list);
-
     return 0;
 }
